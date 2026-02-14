72e2551fa4422fcfa90dca37bd56932868f88420
Jamerson Rodrigo <jamersonrodrigo111@gmail.com>
2026-02-14 10:24:57 -0300

    Create the component Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+export default function Header() {
+  return (
+    <header className="flex justify-center w-full p-6 sticky shadow-md opacity-90">
+      <nav className="w-md flex justify-between font-medium">
+        <a href="*" className="hover:scale-110 transition-all duration-300">
+          Início
+        </a>
+        <a href="*" className="hover:scale-110 transition-all duration-300">
+          Especificações
+        </a>
+        <a href="*" className="hover:scale-110 transition-all duration-300">
+          Detalhes
+        </a>
+        <a href="*" className="hover:scale-110 transition-all duration-300">
+          Footer
+        </a>
+      </nav>
+    </header>
+  );
+}
