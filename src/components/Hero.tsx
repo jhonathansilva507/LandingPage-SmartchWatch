d122ba6aae29fc87bf56b143c0776b921684e00f
Jamerson Rodrigo <jamersonrodrigo111@gmail.com>
2026-02-14 12:12:50 -0300

    Create the component Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,20 @@
+import heroIMG from "../assets/img/hero.jpg";
+
 export default function Hero() {
   return (
     <section>
-      <div></div>
+      <div className="font-contrast flex items-center justify-between container mx-auto pt-2">
+        <div>
+          <h1 className="font-bold text-7xl">
+            Smart Watch <br />
+            Forerunner 55
+          </h1>
+          <span className="text-xl font-contrast">Garmin</span>
+        </div>
+        <div>
+          <img src={heroIMG} alt="Smart Watch" className="w-2xl" />
+        </div>
+      </div>
     </section>
   );
 }
